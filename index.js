7f1c85ccf4de67e107f36d1cd7f116fd657e6820
Vermax <osujinnamdidaniel@OSUJIs-MacBook-Pro.local>
2023-08-14 10:33:28 +0100

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://hms-tes.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
